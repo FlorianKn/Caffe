fc4d99c43de8ef876479c151ef1c5fa0c7e99ba2
Ellen <ellen.bankoivc@gmx.de>
2018-11-14 10:42:43 +0100

    Modify kfkd to convert csv to hd5

--- dataset/kfkd.py
+++ dataset/kfkd.py
@@ -4,7 +4,7 @@ import os
 import numpy as np
 from pandas.io.parsers import read_csv
 from sklearn.utils import shuffle
-
+import h5py
 
 FTRAIN = 'training.csv'
 FTEST = 'test.csv'
@@ -41,9 +41,21 @@ def load(test=False, cols=None):
 
     return X, y
 
+def writeHdf5(t,data,label=None):
+	with h5py.File(os.getcwd()+ '/'+t + '_data.h5', 'w') as f:
+		f['data'] = data
+		if label is not None:
+			f['label'] = label
+	with open(os.getcwd()+ '/'+t + '_data_list.txt', 'w') as f:
+		f.write(os.getcwd()+ '/' +t + '_data.h5\n')
+
 
 X, y = load()
-print("X.shape == {}; X.min == {:.3f}; X.max == {:.3f}".format(
-    X.shape, X.min(), X.max()))
-print("y.shape == {}; y.min == {:.3f}; y.max == {:.3f}".format(
-    y.shape, y.min(), y.max()))
+X = X.reshape((X.shape[0],1,96,96))
+sep = 1600
+writeHdf5('train',X[0:sep],y[0:sep])
+writeHdf5('val',X[sep:],y[sep:])
+
+X,y= load(True)
+X = X.reshape((X.shape[0],1,96,96))
+writeHdf5('test',X,y)
