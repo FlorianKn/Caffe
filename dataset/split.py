d5ba517b49f8e1c230e8b2e3c9d48c4719d448a6
Ellen <ellen.bankoivc@gmx.de>
2019-02-21 19:41:13 +0100

    Update split method

--- dataset/split.py
+++ dataset/split.py
@@ -3,18 +3,21 @@ from sklearn.cross_validation import train_test_split
 import pandas as pd
 
 
-data=pd.read_csv('training.csv')
+data = pd.read_csv('training.csv')
 print data.head()
 
-y = data.Image
-X = data.drop(['Image'], axis=1)
+#y = data.Image
+#X = data.drop(['Image'], axis=1)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.2)
+X_train, X_test = train_test_split(data,test_size=0.2)
 print("\nX_train:\n")
-print(X_train.head())
+print X_train['left_eye_inner_corner_x']
 print(X_train.shape)
 
 print("\nX_test:\n")
 print(X_test.head())
 print(X_test.shape)
+
+#np.savetxt("split_train.csv", X_train, delimiter=",", fmt='%s')
+#np.savetxt("split_test.csv", X_test, delimiter=",")
 
